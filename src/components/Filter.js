25ae047c8ef5326b5b890c99b788450288ebd75d
Fedou Brice <napofedoupro@gmail.com>
2022-09-28 16:19:40 +0200

    Fix btn Filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,8 +3,8 @@ import React from "react";
 const Filter = () => {
   return (
     <div className="filter">
-      <button className="top">top ↑</button>
-      <button className="flop">↓ flop</button>
+      <button className="top">↑ top</button>
+      <button className="flop">flop ↓</button>
     </div>
   );
 };
